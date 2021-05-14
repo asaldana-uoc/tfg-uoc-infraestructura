65caaf4d8f369e20c525a39e7939d7ff44027930
Angel Saldaña Lopez <asaldana@uoc.edu>
2021-05-14 23:38:09 +0200

    Actualitzar Readme i definir versió del mòdul

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# tfg-gitops-infraestructura
+# Repositori per la infraestructura a GCP del TFG 
+
 Repositori per emmagatzemar el codi amb la infrastructura de la plataform
--- development/europe-west1/vpc/main.tf
+++ development/europe-west1/vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 3.0"
+  version = "3.2.2"
 
   project_id   = local.project_id
   network_name = format("%s-%s", local.prefix_name, "vpc")
@@ -8,14 +8,14 @@ module "vpc" {
 
   subnets = [
     {
-      subnet_name           = format("%s-%s", local.prefix_name, "subnet-private")
-      subnet_ip             = "172.17.0.0/23"
-      subnet_region         = local.region
+      subnet_name   = format("%s-%s", local.prefix_name, "subnet-private")
+      subnet_ip     = "172.17.0.0/23"
+      subnet_region = local.region
     },
     {
-      subnet_name           = format("%s-%s", local.prefix_name, "subnet-public")
-      subnet_ip             = "172.18.0.0/23"
-      subnet_region         = local.region
+      subnet_name   = format("%s-%s", local.prefix_name, "subnet-public")
+      subnet_ip     = "172.18.0.0/23"
+      subnet_region = local.region
     }
   ]
 }
